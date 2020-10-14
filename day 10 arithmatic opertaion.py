907acc38ce2f6a31eccf98eca8716312b9040c22
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2020-10-14 11:02:08 +0530

    Create day 10 arithmatic opertaion.py

---
+++ day 10 arithmatic opertaion.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    add=int(a)+int(b)
+    print(add)
+    sub=int(a)-int(b)
+    print(sub)
+    multi=int(a)*int(b)
+    print(multi)
