f8e93a312bb0ef46c6ecd533305123b632041300
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2020-11-08 11:14:29 +0530

    Create day 35 data types.py

---
+++ day 35 data types.py
@@ -0,0 +1,12 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+i_2 = input()
+d_2 = input()
+s_2 = raw_input()
+sum_int = i + i_2
+sum_double = d + d_2
+sum_string = s + s_2
+print sum_int
+print sum_double
+print sum_string
