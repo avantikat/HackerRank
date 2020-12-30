a77a8861eecdc87684cf32123bc12f53bca67425
avantika tellawar <68954508+avantikat@users.noreply.github.com>
2020-12-30 08:52:48 +0530

    Create day83.py

---
+++ day83.py
@@ -0,0 +1,11 @@
+import math
+import os
+import random
+import re
+import sys
+lst=[]
+for a0 in range(int(input())):
+    firstName, emailID = [str(s) for s in input().split()]
+    if re.search('@gmail\.com$', emailID):
+           lst.append(firstName)
+print(*sorted(lst), sep='\n')
